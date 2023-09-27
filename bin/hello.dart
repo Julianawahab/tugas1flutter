018f9b8a02daba969bb0f96f951f072cf25c7207
Julianawahab <145787980+Julianawahab@users.noreply.github.com>
2023-09-27 17:53:46 +0800

    First Commit

---
+++ bin/hello.dart
@@ -0,0 +1,17 @@
+void main(){
+  String nama = "Juliana wahab";
+  int umur = 19;
+  bool menikah = false;
+  double tinggi = 163.50;
+  var alamat = "Banjarmasin, Indonesia";
+
+  nama += "Skom";
+
+  print("Nama saya $nama");
+  print("Umur saya $umur tahun");
+  print("Tinggi saya $tinggi cm");
+  print("menikah $menikah");
+  print("alamat saya $alamat");
+
+
+}
---
+++ bin/if.dart
@@ -0,0 +1,9 @@
+void main(){
+  int usia=19;
+  if (usia>19) {
+    print("Saya masih muda");
+  }else{
+    print("Saya sudah tua");
+  }
+  
+}
